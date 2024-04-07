b303771a2b6dc1007bc16ab4bd2bb510bf233a16
Daelo Namaumbo <bsc-53-16@cc.ac.mw>
2024-04-07 17:29:16 +0200

    css horizontal card 4px; space

--- src/components/horizontalcardComponent/HorizontalCardComponent.js
+++ src/components/horizontalcardComponent/HorizontalCardComponent.js
@@ -20,33 +20,34 @@ export default function HorizontalCardComponent({ product }) {
 
   return (
     <React.Fragment>
-      <Card variant="elevated">
-        <CardBody>
-          <div className="product-details">
-            <div className="product-image">
-              <img
-                src={`${process.env.PUBLIC_URL}/photo.avif`}
-                alt="Green double couch with wooden legs"
-              />
-            </div>
-            <div className="product-info">
-              <div className="product-name">{product.name}</div>
-              <div className="product-color">In stock</div>
-              <div className="product-price">
-                <span className="mtengo">${product.price}</span>/each
+      <div className="horizontal-card-container">
+        <Card variant="elevated">
+          <CardBody>
+            <div className="product-details">
+              <div className="product-image">
+                <img
+                  src={`${process.env.PUBLIC_URL}/photo.avif`}
+                  alt="Green double couch with wooden legs"
+                />
               </div>
-              <Button
-                colorScheme="teal"
-                size="xs"
-                onClick={() => handleRemoveItem(product.name)}
-              >
-                Remove
-              </Button>
+              <div className="product-info">
+                <div className="product-name">{product.name}</div>
+                <div className="product-color">In stock</div>
+                <div className="product-price">
+                  <span className="mtengo">${product.price}</span>/each
+                </div>
+                <Button
+                  colorScheme="teal"
+                  size="xs"
+                  onClick={() => handleRemoveItem(product.name)}
+                >
+                  Remove
+                </Button>
+              </div>
             </div>
-          </div>
-        </CardBody>
-      </Card>
+          </CardBody>
+        </Card>
+      </div>
     </React.Fragment>
   );
 }
-
